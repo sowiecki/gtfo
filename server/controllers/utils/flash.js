40a94547c45c14c829a13f6fb2820706856dcc0a
sowiecki <sowiecki@walmartlabs.com>
2016-01-17 16:14:51 -0600

    Stop runaway strobing.

--- server/controllers/utils/flash.js
+++ server/controllers/utils/flash.js
@@ -1,45 +1,48 @@
 /* eslint no-use-before-define:0 */
 /* globals clearInterval, setInterval */
 import { IN, OUT } from '../../constants/values';
-import { RED, TEAL, GREEN, YELLOW } from '../../constants/colors';
+import { RED, TEAL, GREEN, ORANGE } from '../../constants/colors';
 
 // Keep leds low so as to not disturb occupants
 const faint = 25;
 let flashInterval;
 
-const flash = (led, maxIntensity = 100, rate = 5) => {
-  let fadeDirection = IN;
-  let intensity = maxIntensity;
-
-  flashInterval = setInterval(() => {
-    switch (intensity) {
-      case 0:
-        fadeDirection = IN;
-        break;
-      case maxIntensity:
-        fadeDirection = OUT;
-        break;
-    }
-
-    switch (fadeDirection) {
-      case IN:
-        intensity += 1;
-        break;
-      case OUT:
-        intensity -= 1;
-        break;
-    }
-
-    led.intensity(intensity);
-  }, rate);
-};
+// NOTE May not need this, will probably delete in the future
+// const flash = (led, maxIntensity = 100, rate = 5) => {
+//   let fadeDirection = IN;
+//   let intensity = maxIntensity;
+//
+//   flashInterval = setInterval(() => {
+//     switch (intensity) {
+//       case 0:
+//         fadeDirection = IN;
+//         break;
+//       case maxIntensity:
+//         fadeDirection = OUT;
+//         break;
+//     }
+//
+//     switch (fadeDirection) {
+//       case IN:
+//         intensity += 1;
+//         break;
+//       case OUT:
+//         intensity -= 1;
+//         break;
+//     }
+//
+//     led.intensity(intensity);
+//   }, rate);
+// };
 
 export const vacant = (led) => {
+  led.stop(); // Prevent rogue strobing
   led.intensity(faint);
   led.color(GREEN);
 };
 
 export const occupied = (led) => {
+  led.stop(); // Prevent rogue strobing
   led.intensity(faint);
   led.color(TEAL);
 };
@@ -50,6 +53,6 @@ export const oneMinuteWarning = (led) => {
 };
 
 export const fiveMinuteWarning = (led) => {
-  led.color(YELLOW);
+  led.color(ORANGE);
   led.strobe(200)
 };
