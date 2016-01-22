48fdb2ef7a8b845337b56f69c31d1ee72dec8c1f
sowiecki <sowiecki@walmartlabs.com>
2016-01-22 10:25:35 -0600

    Better green.

--- server/constants/colors.js
+++ server/constants/colors.js
@@ -3,6 +3,6 @@ export const BLUE = '#3399FF';
 export const TEAL = '#217C7E';
 export const DARK_PINK = '#FF0066';
 export const RED = '#FF0000';
-export const GREEN = '#AAF200';
+export const GREEN = '#00CC00';
 export const ORANGE = '#FF2500';
 export const YELLOW = '#FFFF00';
