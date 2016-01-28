a80160a2ccc9e3c323bfefcc317a0bd9c20c3212
Sean Owiecki <sowiecki@walmartlabs.com>
2016-01-27 18:29:42 -0600

    Update path.

--- server/constants/urls.js
+++ server/constants/urls.js
@@ -1,4 +1,5 @@
-const HOST = 'http://localhost:8080';
-// export const HOST = 'http://ec2-54-213-130-244.us-west-2.compute.amazonaws.com:8080/';
+const PRODUCTION_HOST = 'http://ec2-54-213-130-244.us-west-2.compute.amazonaws.com:8080/';
+const DEVELOPMENT_HOST = 'http://localhost:8080';
+const HOST = process.env.NODE_ENV === 'production' ? PRODUCTION_HOST : DEVELOPMENT_HOST;
 
-export const ROOM_RESERVATIONS = `${HOST}/rest/meetingRoom/`;
+export const ROOM_RESERVATIONS = `${HOST}/rest/meetingRoom/lookup/`; // Append Outlook account
