0ca99c22a53920efc8b8289644ffa94f54fc25c5
sowiecki <sowiecki@walmartlabs.com>
2016-01-29 10:09:06 -0600

    Fix URL.

--- server/constants/urls.js
+++ server/constants/urls.js
@@ -1,4 +1,4 @@
-const PRODUCTION_HOST = 'http://ec2-54-175-205-156.compute-1.amazonaws.com:8080/';
+const PRODUCTION_HOST = 'http://ec2-54-175-205-156.compute-1.amazonaws.com:8080';
 const DEVELOPMENT_HOST = 'http://localhost:8080';
 const HOST = process.env.NODE_ENV === 'production' ? PRODUCTION_HOST : DEVELOPMENT_HOST;
 
