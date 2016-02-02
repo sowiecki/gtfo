4ba65f9f2efeb14a8e57e42daa25f6eceeccf44e
Sean Owiecki <sowiecki@walmartlabs.com>
2016-02-01 20:03:14 -0600

    Production webpack changes.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,6 +3,7 @@ var webpack = require('webpack');
 var base = require('./webpack.config');
 
 module.exports = {
+  devtool: 'source-map',
   contet: base.context,
   entry: base.entry,
   output: {
@@ -13,6 +14,16 @@ module.exports = {
   resolve: base.resolve,
   module: base.module,
   plugins: [
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
   ]
 }
