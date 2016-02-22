95b30cc22dc0c52eef3d71a4db2a79b981da77fb
sowiecki <sowiecki@walmartlabs.com>
2016-02-22 15:52:35 -0600

    Use separate SVG for anim to avoid colors breaking.

--- client/components/layout/meeting-room.jsx
+++ client/components/layout/meeting-room.jsx
@@ -7,25 +7,30 @@ import { styles, rules, TEXT_DX, TEXT_DY } from './styles';
 import { positionModifier, shapeModifier } from '../../utils/rooms';
 import { OFFLINE,
          PINGED,
-         RECT_ANIMATION_LOOPS,
-         RECT_ANIMATION_TIMEOUT } from '../../constants/svg';
+         PING_ANIMATION_LOOPS,
+         PING_ANIMATION_TIMEOUT } from '../../constants/svg';
 
 const MeetingRoom = ({ room, pinged }) => {
-  const rectAnimation = {
-    fill: styles[!pinged ? (room.alert || OFFLINE) : PINGED]
+  const pingAnimation = {
+    fill: styles[PINGED],
+    opacity: pinged ? 1 : 0
   };
+
+  const pingLoop = pinged ? PING_ANIMATION_LOOPS : 0;
 
   return (
     <svg {...positionModifier(room.coordinates)}>
       <Style rules={rules.roomText}/>
+      <rect
+        fill={styles[room.alert || OFFLINE]}
+        stroke={styles.svgStroke}
+        {...shapeModifier(room.coordinates)}/>
       <VelocityComponent
-        animation={rectAnimation} loop={RECT_ANIMATION_LOOPS}
-        duration={RECT_ANIMATION_TIMEOUT}>
-          <rect
-            fill={styles[room.alert || OFFLINE]}
-            stroke={styles.svgStroke}
-            {...shapeModifier(room.coordinates)}>
-          </rect>
+        animation={pingAnimation}
+        loop={pingLoop}
+        duration={PING_ANIMATION_TIMEOUT}
+        style={{stroke: styles.svgStroke}}>
+          <rect {...shapeModifier(room.coordinates)}/>
       </VelocityComponent>
       <text
         className='room-text'
