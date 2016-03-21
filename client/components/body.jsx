aa9567a2b37e303347ca62eab63aabf084fff8c8
sowiecki <sowiecki@walmartlabs.com>
2016-03-20 21:19:52 -0500

    Re-enable fullscreen with query param.

--- client/components/body.jsx
+++ client/components/body.jsx
@@ -7,17 +7,28 @@ import { base } from '../config/composition';
 import { rules } from './common/styles';
 
 const Body = (props) => {
+  const isFullscreen = props.location.query.fullscreen === 'true';
+  // TODO provide some small button to return from fullscreen
+  const navigation = isFullscreen ? null : (
+    <NavigationContainer {...props}/>
+  );
+
   return (
     <StyleRoot>
       <Style rules={rules.body}/>
-      <NavigationContainer {...props}/>
+      {navigation}
       {props.children}
     </StyleRoot>
   );
 };
 
 Body.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  location: PropTypes.shape({
+    query: PropTypes.shape({
+      fullscreen: PropTypes.bool
+    })
+  })
 };
 
 export default base(Body);
