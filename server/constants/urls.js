8fce103a270161277b4b84c9288650fe285ac3c9
sowiecki <sowiecki@walmartlabs.com>
2016-03-20 19:00:46 -0500

    Fix prod host path.

--- server/constants/urls.js
+++ server/constants/urls.js
@@ -1,6 +1,6 @@
 const ENV_PATH = '../environment';
 const DEVELOPMENT_HOST = 'http://localhost:8080';
 const isProd = process.env.NODE_ENV === 'production';
-const HOST = isProd ? require(ENV_PATH).productionHost : DEVELOPMENT_HOST;
+const HOST = isProd ? require(ENV_PATH).config.productionHost : DEVELOPMENT_HOST;
 
 export const ROOM_RESERVATIONS = `${HOST}/rest/meetingRoom/lookup/`; // Append Outlook account
