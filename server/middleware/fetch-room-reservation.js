b673c126950a7ef7ae5b4a801097164b1923b9b3
Sean Owiecki <sowiecki@walmartlabs.com>
2016-03-29 19:49:34 -0500

    Move logic from API to reducer.

--- server/middleware/fetch-room-reservation.js
+++ server/middleware/fetch-room-reservation.js
@@ -4,7 +4,6 @@ import http from 'http';
 
 import { EMIT_ROOM_STATUSES_UPDATE } from '../ducks/rooms';
 import * as urls from '../constants';
-import { filterExpiredReservations, getRoomAlert } from '../utils';
 
 const fetchRoomReservations = (next, action) => {
   const { room, accessories } = action;
@@ -13,14 +12,12 @@ const fetchRoomReservations = (next, action) => {
   // Retrieve room reservation statuses from Exchange wrapper
   http.get(source, (response) => {
     response.on('data', (data) => {
-      const reservationsResponse = JSON.parse(data.toString('utf8'));
-      const reservations = filterExpiredReservations(reservationsResponse);
-      const alert = getRoomAlert(reservations);
+      const reservations = JSON.parse(data.toString('utf8'));
 
       next({
         type: EMIT_ROOM_STATUSES_UPDATE,
         room,
-        alert,
+        reservations,
         accessories
       });
     });
