f37c13c63c4d54877f58eaed9e98aa6277692524
Sean Owiecki <sowiecki@walmartlabs.com>
2016-04-10 17:00:37 -0500

    Remove obsolete dep.

--- client/application.jsx
+++ client/application.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, IndexRoute, Route } from 'react-router';
 import { Provider } from 'react-redux';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import history from './config/history';
 import store from './store';
@@ -13,8 +12,6 @@ import LayoutContainer from './components/layout/container';
 
 require('velocity-animate');
 require('velocity-animate/velocity.ui');
-
-injectTapEventPlugin();
 
 const node = document.getElementById('root');
 
