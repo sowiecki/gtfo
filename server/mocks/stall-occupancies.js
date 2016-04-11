22b0fdc114395fc08bd886d22174ee9957f8d2ac
Sean Owiecki <sowiecki@walmartlabs.com>
2016-04-10 20:48:32 -0500

    Shuffle stall color.

--- server/mocks/stall-occupancies.js
+++ server/mocks/stall-occupancies.js
@@ -1,8 +1,8 @@
-import { OCCUPIED, VACANT } from '../constants';
+import { SQUATTED, VACANT } from '../constants';
 
 const getMockStallOcuppancies = () => (
   [
-    { id: 'mensStall1', alert: OCCUPIED, location: 'two-prudential-51' },
+    { id: 'mensStall1', alert: SQUATTED, location: 'two-prudential-51' },
     { id: 'mensStall2', alert: VACANT, location: 'two-prudential-51' }
   ]
 );
