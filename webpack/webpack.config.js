aead58b881c6a89401d43321791738b80536ebfe
sowiecki <sowiecki@walmartlabs.com>
2016-04-23 20:41:28 -0500

    Alias dev packages to null.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,33 @@
 /* globals __dirname */
 const path = require('path');
+const merge = require('lodash/merge');
 
 const baseContext = path.join(__dirname, '../client');
 const environmentConext = path.join(__dirname, '../environment');
 const universalContext = path.join(__dirname, '../universal');
+
+const generateAlias = (result, developmentModule) => {
+  const isProd = process.env.NODE_ENV === 'production';
+
+  result[developmentModule] = isProd ? null : developmentModule;
+
+  return result;
+};
+
+/**
+ * Development-only modules listed here will be aliased to null,
+ * to prevent them from being used in production.
+ */
+const developmentModules = [
+  'redux-devtools',
+  'redux-devtools-dock-monitor',
+  'redux-devtools-log-monitor',
+  'redux-slider-monitor',
+  'redux-devtools-chart-monitor',
+  'redux-devtools-diff-monitor',
+  'redux-devtools-inspector',
+  'redux-devtools-dispatch'
+].reduce(generateAlias, {});
 
 module.exports = {
   context: baseContext,
@@ -15,9 +39,9 @@ module.exports = {
   },
   resolve: {
     root: baseContext,
-    alias: {
+    alias: merge({
       universal: path.resolve(universalContext)
-    },
+    }, developmentModules),
     extensions: ['', '.js', '.jsx']
   },
   module: {
