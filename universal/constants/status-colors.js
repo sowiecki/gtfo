80480926ceb8e7c224dfee8d738977961868fd80
sowiecki <sowiecki@walmartlabs.com>
2016-05-03 14:42:11 -0500

    Resolve linting issue.

--- universal/constants/status-colors.js
+++ universal/constants/status-colors.js
@@ -9,14 +9,11 @@ import { OCCUPIED,
 
 const PURPLE = '#9800FF';
 const BLUE = '#3399FF';
-const TEAL = '#217C7E';
-const DARK_PINK = '#FF0066';
 const RED = '#FF0000';
 const GREEN = '#00CC00';
-const LIGHT_GREEN = '#72E572';
 const ORANGE = '#FFAE00';
 const YELLOW = '#FFFF00';
-const GREY = '#D3D3D3'
+const GREY = '#D3D3D3';
 
 export const STATUS_COLORS = {
   [OCCUPIED]: BLUE,
