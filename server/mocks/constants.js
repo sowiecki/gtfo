86b6629d0d1eae3b9d68eadbbde7d665404f4af3
SeanOwiecki <seanowiecki@gmail.com>
2016-05-08 13:20:15 -0500

    Update deps.

--- server/mocks/constants.js
+++ server/mocks/constants.js
@@ -3,5 +3,9 @@ import moment from 'moment';
 export const MOCK_DATA_FILE = './environment/mock-data.json';
 export const GAP_PROBABILITY = 0;
 export const RESERVATIONS_PER_DAY = 14;
+
 const START_HOUR = 9;
-export const START_OF_DAY = moment().startOf('day').add(START_HOUR, 'hours').toISOString();
+export const START_OF_DAY = moment()
+  .startOf('day')
+  .add(START_HOUR, 'hours')
+  .toISOString();
