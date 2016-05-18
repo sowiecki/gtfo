706b3d56c79dc1c2b1d2a2f4ec482d2de2cfbef5
SeanOwiecki <seanowiecki@gmail.com>
2016-05-17 22:53:51 -0500

    ipad and mobile vqa.

--- client/components/common/styles/rules.js
+++ client/components/common/styles/rules.js
@@ -4,17 +4,20 @@ import { colors } from './base';
 export const rules = {
   body: {
     'html, body, #root, #root > div': {
+      position: 'absolute',
       margin: '0',
       height: '100%',
       width: '100%',
+      maxHeight: '100%',
+      maxWidth: '100%',
       overflow: 'hidden',
       backgroundColor: colors.GHOST_WHITE
     },
 
-    ...transitions
-  },
+    '*': {
+      boxSizing: 'border-box'
+    },
 
-  '*': {
-    boxSizing: 'border-box'
+    ...transitions
   }
 };
