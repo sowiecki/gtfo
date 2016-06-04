ee8c5eae698007d9a36a516b25ed532432e240c1
sowiecki <sowiecki@walmartlabs.com>
2016-06-03 23:24:32 -0500

    Fix Time Travel component test.

--- tests/client/components/navigation/time-travel.spec.jsx
+++ tests/client/components/navigation/time-travel.spec.jsx
@@ -2,28 +2,49 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import expect from 'expect';
+import sinon from 'sinon';
 
 import { VelocityComponent } from 'velocity-react';
 import Card from 'material-ui/Card/Card';
 import Slider from 'material-ui/Slider';
 
+import { MAX_TIME } from 'constants';
 import TimeTravel from 'components/navigation/time-travel';
 import { provideMuiTheme } from 'config/composition';
 
 describe('<TimeTravel/>', () => {
+  const clock = (time) => sinon.useFakeTimers(Date.parse(time), 'Date');
+
   const props = {
     actions: {
       emitTimeTravelUpdate: () => {},
       emitTimeTravelControlsToggle: () => {},
-      emitTimeSliderValueUpdate: () => {}
+      emitTimeSliderValueUpdate: () => {},
     },
+    onTimeTravelDismissClick: () => {},
     timeTravelledTo: null,
     timeTravelControlsOpen: false
   };
 
-  const component = mount(provideMuiTheme(<TimeTravel {...props}/>));
+  beforeEach(() => {
+    sinon.useFakeTimers().restore();
+  });
 
-  it('renders a <Slider/> component inside of a <VelocityComponent/>.', () => {
+  afterEach(() => {
+    sinon.useFakeTimers().restore();
+  });
+
+  it(`renders a <Slider/> component inside of a <VelocityComponent/> before ${MAX_TIME}.`, () => {
+    clock('Tuesday, March 8, 2016 3:00 PM CST');
+    const component = mount(provideMuiTheme(<TimeTravel {...props}/>));
+
     expect(component.find(VelocityComponent).find(Card).find(Slider).length).toEqual(1);
+  });
+
+  it(`renders no <Slider/> component after ${MAX_TIME}.`, () => {
+    clock('Tuesday, March 8, 2016 8:01 PM CST');
+    const component = mount(provideMuiTheme(<TimeTravel {...props}/>));
+
+    expect(component.find(VelocityComponent).find(Card).find(Slider).length).toEqual(0);
   });
 });
