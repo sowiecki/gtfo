05fcc7b681fe3e4715b85f085fe29e9653ad70de
sowiecki <sean.owiecki@gmail.com>
2016-10-30 22:35:38 -0500

    Clean up containers, update deps

--- client/components/layout/container.js
+++ client/components/layout/container.js
@@ -1,35 +1,9 @@
-import React, { Component, PropTypes } from 'react';
-import ImmutablePropTypes from 'immutable-props';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import RoomsController from './controller';
 
 import * as RoomsActions from '../../ducks/layout';
-
-class LayoutContainer extends Component {
-  componentWillMount() {
-    const { actions, location } = this.props;
-
-    actions.connectSocket(location.query);
-  }
-
-  render() {
-    return (
-      <RoomsController {...this.props}/>
-    );
-  }
-}
-
-LayoutContainer.propTypes = {
-  layout: ImmutablePropTypes.Map,
-  location: PropTypes.object.isRequired,
-  history: PropTypes.object.isRequired,
-  actions: PropTypes.object.isRequired,
-  route: PropTypes.object.isRequired,
-  routeParams: PropTypes.object.isRequired,
-  params: PropTypes.object.isRequired
-};
 
 const mapStateToProps = ({ layoutReducer }) => ({
   error: layoutReducer.get('error'),
@@ -50,4 +24,4 @@ const mapDispatchToProps = (dispatch) => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(LayoutContainer);
+)(RoomsController);
