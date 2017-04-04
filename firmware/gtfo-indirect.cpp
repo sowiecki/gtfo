04be3e43c064558866c757de6557f0d64cfeafa0
sowiecki <nase00@gmail.com>
2017-04-04 16:03:16 -0500

    Update firmware for new statuses

--- firmware/gtfo-indirect.cpp
+++ firmware/gtfo-indirect.cpp
@@ -2,11 +2,16 @@
 using namespace RGBControls;
 
 Led led(A7, A5, A4, false);
+int motionSensorPin = A0;
+int fadeRate = 2000;
+String id;
 
 Color standby1(10, 10, 10);
 Color standby2(50, 50, 50);
 
 Color purple(150, 0, 150);
+Color purpleSoft(150, 25, 150);
+
 Color green(0, 255, 0);
 Color greenSoft(25, 255, 25);
 
@@ -19,11 +24,23 @@ Color blueSoft(50, 50, 255);
 
 Color colors[2] = { standby1, standby2 };
 
-int fadeRate = 2000;
+void getDeviceInfo(const char *topic, const char *data) {
+  Serial.println("received " + String(topic) + ": " + String(data));
+  id = String(data);
+  Particle.publish("spark/device/name");
+}
 
 void setup() {
   led.setColor(standby1);
   Particle.function("status", handleStatus);
+
+  Serial.begin(115200);
+  for(int i=0;i<5;i++) {
+    Serial.println("waiting... " + String(5 - i));
+    delay(1000);
+  }
+  Particle.subscribe("spark/", getDeviceInfo);
+  Particle.publish("spark/device/name");
 }
 
 int handleStatus(String status) {
@@ -50,6 +67,16 @@ int handleStatus(String status) {
     colors[0] = red;
     colors[1] = orange;
     return 1;
+  } else if (status == "SQUATTED") {
+    fadeRate = 750;
+    colors[0] = purple;
+    colors[1] = purpleSoft;
+    return 1;
+  } else if (status == "ABANDONED") {
+    fadeRate = 500;
+    colors[0] = blue;
+    colors[1] = green;
+    return 1;
   }
 
   return 0;
@@ -57,4 +84,12 @@ int handleStatus(String status) {
 
 void loop() {
   led.fade(colors, 2, fadeRate);
+
+  for(int i = 0; i < 100; i += 1) {
+    int analogMotionValue = analogRead(motionSensorPin);
+    if (analogMotionValue > 1000) {
+      Particle.publish("MOTION_DETECTED", id);
+    }
+    delay(10);
+  }
 }
