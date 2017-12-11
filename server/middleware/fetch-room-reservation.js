91185734cb2861ae14cba362e29cbc7ff25caba1
sowiecki <nase00@gmail.com>
2017-12-10 20:24:49 -0600

    Code formatting

--- server/middleware/fetch-room-reservation.js
+++ server/middleware/fetch-room-reservation.js
@@ -4,25 +4,27 @@ import { EMIT_RESERVATIONS_UPDATE } from '../ducks/rooms';
 import { RESERVATIONS_URL } from '../constants';
 import { formatReservations, logfetchRoomReservationError } from '../utils';
 
-const fetchRoomReservation = (next) => {
+const fetchRoomReservation = next => {
   let body = '';
 
   // Retrieve room reservation statuses from external service
-  http.get(RESERVATIONS_URL, (response) => {
-    response.on('data', (data) => {
-      body += data;
-    });
-
-    response.on('end', () => {
-      const parsedData = JSON.parse(body.toString('utf8'));
-      const reservations = formatReservations(parsedData);
-
-      next({
-        type: EMIT_RESERVATIONS_UPDATE,
-        reservations
+  http
+    .get(RESERVATIONS_URL, response => {
+      response.on('data', data => {
+        body += data;
       });
-    });
-  }).on('error', logfetchRoomReservationError);
+
+      response.on('end', () => {
+        const parsedData = JSON.parse(body.toString('utf8'));
+        const reservations = formatReservations(parsedData);
+
+        next({
+          type: EMIT_RESERVATIONS_UPDATE,
+          reservations,
+        });
+      });
+    })
+    .on('error', logfetchRoomReservationError);
 };
 
 export default fetchRoomReservation;
