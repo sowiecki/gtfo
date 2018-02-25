8ee4f14651619dd0f005aa4e10918fc891e2556d
sowiecki <nase00@gmail.com>
2018-02-25 15:33:06 -0600

    Test genURL

---
+++ tests/server/utils/api.spec.js
@@ -0,0 +1,33 @@
+/* eslint-env node, mocha */
+/* eslint no-magic-numbers:0 max-nested-callbacks:0 */
+import expect from 'expect';
+
+import { genURL } from 'server/utils';
+
+describe('API utilities', () => {
+  const mockConfig = {
+    reservations: {
+      host: 'foo',
+      path: '/bar'
+    },
+    stalls: {
+      host: 'bizz',
+      path: ''
+    },
+    proxy: {
+      host: 'bazz'
+    }
+  };
+
+  describe('genURL', () => {
+    it('should generate URLs from tenant host and path', () => {
+      const reservationsResult = genURL(mockConfig.reservations);
+      const stallsResult = genURL(mockConfig.stalls);
+      const proxyResult = genURL(mockConfig.proxy);
+
+      expect(reservationsResult).toEqual('foo/bar');
+      expect(stallsResult).toEqual('bizz'); // Tests empty path
+      expect(proxyResult).toEqual('bazz'); // Tests undefined path
+    });
+  });
+});
