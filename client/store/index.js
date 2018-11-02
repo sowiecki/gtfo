b43c0eadce7e170f3f01fb8113ba1fb12adcc52a
sowiecki <nase00@gmail.com>
2018-11-02 12:45:41 -0500

    Remove obsolete code.

--- client/store/index.js
+++ client/store/index.js
@@ -1,21 +1,10 @@
 /* eslint no-underscore-dangle:0 */
-/* globals window */
 import { applyMiddleware, compose, createStore } from 'redux';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 
 import history from '../config/history';
 import rootReducer from '../ducks';
 import api from '../middleware/api';
-
-// const historyMiddleware = routerMiddleware(history);
-// const baseMiddleware = applyMiddleware(api, historyMiddleware);
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// const generateStore = (initialState = {}) => {
-//   const composeStore = composeEnhancers(baseMiddleware)(createStore);
-
-//   return composeStore(rootReducer, initialState);
-// };
 
 const generateStore = (initialState = {}) => createStore(
   connectRouter(history)(rootReducer), // new root reducer with router state
