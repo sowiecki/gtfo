dd7e1dbb3c7b6104a2311c8c5a3444333c9cc87c
sowiecki <nase00@gmail.com>
2018-11-13 16:31:36 -0600

    Fix annoying typography error.

--- client/components/body/index.jsx
+++ client/components/body/index.jsx
@@ -1,9 +1,13 @@
+/* global window */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 import NavigationContainer from '../navigation/container';
 
 import './styles';
+
+// https://material-ui.com/style/typography/#migration-to-typography-v2
+window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true; // eslint-disable-line
 
 const Body = (props) => <NavigationContainer {...props}>{props.children}</NavigationContainer>;
 
