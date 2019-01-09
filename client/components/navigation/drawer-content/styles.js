6274fbc0a7b1941b9236feed0683ebfc4e96ff34
sowiecki <nase00@gmail.com>
2019-01-09 17:36:51 -0600

    Fix minor style issue with pointer on drawer-content.

--- client/components/navigation/drawer-content/styles.js
+++ client/components/navigation/drawer-content/styles.js
@@ -6,22 +6,19 @@ const stylesGenerator = () => ({
   base: css`
     height: 100%;
     width: 300px;
-    cursor: pointer;
     background-color: ${colors.DARK_GREY};
-
-    :last-child {
-      cursor: default;
-    }
 
     > li {
       background-color: ${colors.DARK_GREY};
       transition: all 120ms ease-in-out;
+      cursor: pointer;
 
       :hover {
         background-color: ${colors.primary};
 
         :last-child {
           background-color: ${colors.DARK_GREY};
+          cursor: default;
         }
       }
 
