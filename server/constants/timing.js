252dbbeb93aec8343c68dbd23b366724cefd24f6
sowiecki <nase00@gmail.com>
2019-02-21 21:56:23 -0600

    Bump timers.

--- server/constants/timing.js
+++ server/constants/timing.js
@@ -1,9 +1,3 @@
-export const ONE_MINUTE_STROBE = 200; // milliseconds
-export const FIVE_MINUTE_STROBE = 400; // milliseconds
-export const FAINT_LIGHT_LEVEL = 25;
-export const FULL_LIGHT_LEVEL = 100;
-
-export const RESERVATIONS_CHECK_INTERVAL = 10000;
-export const STALLS_CHECK_INTERVAL = 5000;
-export const MOTION_GRACE_PERIOD = 15; // seconds
-export const SPINNER_DELAY = 3000;
+export const RESERVATIONS_CHECK_INTERVAL = 60000; // 1 minute
+export const STALLS_CHECK_INTERVAL = 30000; // 30 seconds
+export const MOTION_GRACE_PERIOD = 15; // 15 seconds
