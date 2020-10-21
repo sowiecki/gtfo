2ffb2633242b9355aa46e89e686d84d1b2e31c2a
Nase00 <nase00@gmail.com>
2020-10-21 17:37:32 -0500

    Fix schemas (#333)
    * Fix schemas not validating correctly.
    
    * Revert node downgrade.
    
    Co-authored-by: seanowiecki <me@seanowiecki.com>

--- environment/validation.js
+++ environment/validation.js
@@ -1,20 +1,5 @@
 import { Validator } from 'jsonschema';
-import { forEach } from 'lodash';
-
-import deviceSchema from './schemas/device';
-import markerSchema from './schemas/marker';
-import coordinateSchema from './schemas/coordinate';
-import filesSchemas from './schemas/files';
 
 const validator = new Validator();
-
-const schemas = {
-  deviceSchema,
-  markerSchema,
-  coordinateSchema,
-  ...filesSchemas
-};
-
-forEach(schemas, (schema) => validator.addSchema(schema, schema.id));
 
 export default validator;
