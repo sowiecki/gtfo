a631d438e88616abf71bb28ce1ba80f9a320b3a4
seanowiecki <me@seanowiecki.com>
2020-10-27 16:07:56 -0500

    Temporarily switch to unofficial Enzyme 17 adapter.

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,6 +1,7 @@
 /* eslint-env node, jest */
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+// import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'; // TODO swap back to official adapter when it's released
 
 // https://material-ui.com/style/typography/#migration-to-typography-v2
 window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true; // eslint-disable-line
